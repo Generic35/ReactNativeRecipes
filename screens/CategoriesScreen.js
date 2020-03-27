2e7a4cbf5237c22521fc229103c89566036bd1a3
Eric Lamontagne <eric.lamontagne@mphasis.com>
2020-03-27 12:22:08 -0400

    (feature) Scaffold screens and add font loading

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const CategoriesScreen = props => {
+  return <View style={styles.screen}></View>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default CategoriesScreen;
